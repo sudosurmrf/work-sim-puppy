f15e6f644541c591d5f1364daeebf4fde15a81dd
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-07-15 10:44:19 -0700

    took out comment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
       
       const apiPull = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2406-FTB-ET-WEB-FT/players');
       const response = await apiPull.json();
-      console.log(response.data.players); //grabs the entire list of players
+      // console.log(response.data.players); //grabs the entire list of players
       // console.log(response.data.players[0].name); //grabs the name of the specific indexed player
       // console.log(response.data.players);
       let players = response.data.players;
